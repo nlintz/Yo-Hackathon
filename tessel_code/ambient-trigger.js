bd12cd14d799bfc1daa9661aa695d99987b13aca
Nathan Lintz <nlintz@gmail.com>
2014-06-27 20:12:06 -0700

    camera not working

--- tessel_code/ambient-trigger.js
+++ tessel_code/ambient-trigger.js
@@ -4,17 +4,25 @@ var ambient = ambientlib.use(tessel.port['A']);
 var camera = require('camera-vc0706').use(tessel.port['B']); 
 var notificationLED = tessel.led[3]; // Set up an LED to notify when we're taking a picture
 
+camera.on('ready', function () {
+  cameraReady = true;
+})
+
 function ambientTrigger (callback) {
-  var cb = callback;
-  ambient.on('ready', function () {
+  ambient.on('ready', function (err) {
+
     ambient.setSoundTrigger(0.1);
 
     ambient.on('sound-trigger', function(data) {
-      callback(data);
+      console.log(data, callback)
+      if (data != undefined) {
+        callback(data)
+      };
+      
       ambient.clearSoundTrigger();
 
       setTimeout(function () {
-          ambient.setSoundTrigger(0.1, cb);
+          ambient.setSoundTrigger(0.1);
       }, 1500);
     });
   });
@@ -23,21 +31,24 @@ function ambientTrigger (callback) {
 // EXAMPLE CODE
 ambientTrigger(function (data) {
   console.log("Warning! Activity Detected. Level:", data)
-  camera.takePicture(function(err, image) {
-    notificationLED.high();
-    if (err) {
-      console.log('error taking image', err);
-    } else {
-      notificationLED.low();
-      // Name the image
-      var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
-      // Save the image
-      process.sendfile(name, image);
-      // Turn the camera off to end the script
-      camera.disable();
-      notificationLED.low();
-    }
-  })
+  if (cameraReady) 
+  {
+    camera.takePicture(function(err, image) {
+      notificationLED.high();
+      if (err) {
+        console.log('error taking image', err);
+      } else {
+        notificationLED.low();
+        // Name the image
+        var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
+        // Save the image
+        process.sendfile(name, image);
+        // Turn the camera off to end the script
+        // camera.disable();
+        notificationLED.low();
+      }
+    })
+  }
 
 });
 
